45afdc485110445f9b5220230392f569ce22b6f8
Jemaul Hunter <jemaulhunter@gmail.com>
2022-03-28 10:21:12 -0400

    commit tree 71d9383f818857dced8cbb1a91bf16f8596fa353

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    return string.toUpperCase()
+}
+function logWhisper(string) {
+    return string.toLowerCase()
+}
+function sayHiToHeadphonedRoommate(string) {
+    if(string === string.toLowerCase()) {
+        return "I can't hear you!"
+    }
+    else if (string === string.toUpperCase())
+    return "YES INDEED!"
+}
+    else if (string === "I would love to!"){
+        return "Let's have dinner together."
+    }
+}
